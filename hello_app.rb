2dbc6af29f40d993ae60334e5711738f58eec983
skibox <m.skibicki3007@gmail.com>
2018-12-20 12:41:41 +0100

    Time

--- hello_app.rb
+++ hello_app.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 
 get '/' do
-	'bye bye'	
+	DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+              "Thursday", "Friday", "Saturday"]	
+    dayname = DAYNAMES[Time.now.wday]
+    "Happy #{dayname}."
 end
