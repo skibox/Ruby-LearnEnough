c5f814c89a81c5637bf0030c066ecd8731dee983
skibox <m.skibicki3007@gmail.com>
2018-12-20 12:44:21 +0100

    Add second site /date

--- hello_app.rb
+++ hello_app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require 'date'
 
 get '/' do
 	DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
               "Thursday", "Friday", "Saturday"]	
     dayname = DAYNAMES[Time.now.wday]
     "Happy #{dayname}."
+end
+
+get '/date' do
+  dayname = Date::DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}."
 end
