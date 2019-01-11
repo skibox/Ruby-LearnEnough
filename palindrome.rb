47bdb065401d27c5a9a83fc6ffbeefc88965107d
skyy <m.skibicki3007@gmail.com>
2019-01-11 12:36:59 +0100

    Class inheritance

--- palindrome.rb
+++ palindrome.rb
@@ -1,8 +1,53 @@
-def palindrome?(string)
-    string_downcase = string.downcase
-    string_downcase == string_downcase.reverse
-end
+# frozen_string_literal: true
 
 def email_parts(email)
-    email.downcase.split('@')
+  email.downcase.split('@')
 end
+
+class String
+  def processor(string)
+    downcase
+  end
+
+  def palindrome?
+    content_downcase == content_downcase.reverse
+  end
+
+  def louder
+    upcase
+  end
+  
+  def blank?
+    strip.length.zero?
+  end
+
+  private
+
+  def content_downcase
+    processor(self)
+  end
+end
+
+class TranslatedPhrase < String
+  attr_accessor :translation
+
+  def initialize(content, translation)
+    super(content)
+    @translation = translation
+  end
+
+  def content_downcase
+    processor(translation)
+  end
+end
+
+phrase = String.new('Racecar')
+puts phrase
+puts phrase.palindrome?
+puts phrase.louder
+
+frase = TranslatedPhrase.new('recognize', 'reconocer')
+puts frase.palindrome?
+
+blank = String.new("a     ")
+p blank.blank?
